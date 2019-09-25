7f15b37aedfe3dc7c7b2d5fc96ed58f6809c6ddf
Sean Chang <seantc@uci.edu>
2019-09-25 01:36:59 -0700

    Updates

---
+++ precache-manifest.a6f1612d730e24409bb88c76b0a7e0d9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef987a728b8844d2b25f6c58207b743",
+    "url": "/CKI-Website/index.html"
+  },
+  {
+    "revision": "e40646d55e1bc306b2f5",
+    "url": "/CKI-Website/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c395f2591868570b2965",
+    "url": "/CKI-Website/static/js/2.5a323800.chunk.js"
+  },
+  {
+    "revision": "e40646d55e1bc306b2f5",
+    "url": "/CKI-Website/static/js/main.6b4765f1.chunk.js"
+  },
+  {
+    "revision": "1756f125e84da967184b",
+    "url": "/CKI-Website/static/js/runtime~main.fd8465ed.js"
+  },
+  {
+    "revision": "a77f9b55c97f225100b9bf5498c05266",
+    "url": "/CKI-Website/static/media/Anoop.a77f9b55.jpg"
+  },
+  {
+    "revision": "ac700db739b66b7897e11ffead56bace",
+    "url": "/CKI-Website/static/media/CKI_Word.ac700db7.jpg"
+  },
+  {
+    "revision": "fb4b4d985b75e3ea083c5567e80c4b43",
+    "url": "/CKI-Website/static/media/District_Emblem.fb4b4d98.png"
+  },
+  {
+    "revision": "13fa79bb9b002968d729228f52db1eea",
+    "url": "/CKI-Website/static/media/Gian.13fa79bb.jpg"
+  },
+  {
+    "revision": "860a41c693833f125aa4a400c0ea2294",
+    "url": "/CKI-Website/static/media/Ishan.860a41c6.jpg"
+  },
+  {
+    "revision": "856e5f30a4199044101a768658df974a",
+    "url": "/CKI-Website/static/media/Jeevan.856e5f30.jpg"
+  },
+  {
+    "revision": "22e8c6205d3961f9012095ddfb323c66",
+    "url": "/CKI-Website/static/media/Jonnie.22e8c620.jpg"
+  },
+  {
+    "revision": "ad2b947a6dec5ad975dd92c8de842611",
+    "url": "/CKI-Website/static/media/Justin.ad2b947a.jpg"
+  },
+  {
+    "revision": "ccfadc0da8fbdfaac16deb7635e80503",
+    "url": "/CKI-Website/static/media/Kevin.ccfadc0d.jpg"
+  },
+  {
+    "revision": "4712b597439fb1f089d94ebf27732643",
+    "url": "/CKI-Website/static/media/Luke.4712b597.jpg"
+  },
+  {
+    "revision": "8059e29c5a7c9d8df33f900c48b2b8ba",
+    "url": "/CKI-Website/static/media/Nat.8059e29c.jpg"
+  },
+  {
+    "revision": "bd42638a3d44bd3bc9813f0df6b1d60b",
+    "url": "/CKI-Website/static/media/Pres.bd42638a.jpg"
+  },
+  {
+    "revision": "df82c5ecbd920c078ed75fe13d795ee6",
+    "url": "/CKI-Website/static/media/bottom-nav.df82c5ec.PNG"
+  },
+  {
+    "revision": "23d5159ccaace3aa13ccfc899a8e6cd5",
+    "url": "/CKI-Website/static/media/circlek.23d5159c.png"
+  },
+  {
+    "revision": "e290096d2cc8d580c5a5f07720122d11",
+    "url": "/CKI-Website/static/media/cki_group_pic.e290096d.jpg"
+  },
+  {
+    "revision": "73467f53bdad0200047ef6ad5e8c0000",
+    "url": "/CKI-Website/static/media/everyone.73467f53.jpg"
+  },
+  {
+    "revision": "e6bd996965ba932342e3bbc3ae3352d2",
+    "url": "/CKI-Website/static/media/icon_fellowship_Black.e6bd9969.png"
+  },
+  {
+    "revision": "e4c286cc5c6b831462c50e6af356bd2a",
+    "url": "/CKI-Website/static/media/icon_leadership_Black.e4c286cc.png"
+  },
+  {
+    "revision": "ad57644a17f9ae50a143d8407a8a3d13",
+    "url": "/CKI-Website/static/media/icon_service_Black.ad57644a.png"
+  },
+  {
+    "revision": "9651f39cc8c3c1d4380f52dac1ac85df",
+    "url": "/CKI-Website/static/media/icon_sunset.9651f39c.png"
+  },
+  {
+    "revision": "623c6694122c4fff718ba96d6023f4e3",
+    "url": "/CKI-Website/static/media/join_us.623c6694.jpg"
+  },
+  {
+    "revision": "939553d973c94f031edb9e74121a90fb",
+    "url": "/CKI-Website/static/media/sunset_emblem.939553d9.png"
+  }
+]);
